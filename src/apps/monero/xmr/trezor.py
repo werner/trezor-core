f696cc51026aa2c594b28550ae6c3e4b222e7685
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-02 16:36:07 +0200

    xmr: trezor code fix

--- src/apps/monero/xmr/trezor.py
+++ src/apps/monero/xmr/trezor.py
@@ -2,15 +2,12 @@
 # -*- coding: utf-8 -*-
 # Author: Dusan Klinec, ph4r05, 2018
 
-import binascii
+import ubinascii as binascii
 
 from apps.common import seed
 from apps.monero.xmr import crypto
 from apps.monero.xmr import monero
-from apps.wallet.get_address import _show_address, _show_qr
-
 from trezor.crypto.hashlib import sha256
-from trezor.messages.MoneroAddress import MoneroAddress
 
 MONERO_CURVE = 'ed25519-keccak'
 
@@ -18,9 +15,9 @@ MONERO_CURVE = 'ed25519-keccak'
 async def monero_get_creds(ctx, address_n=None, network_type=None):
     address_n = address_n or ()
     node = await seed.derive_node(ctx, address_n, MONERO_CURVE)
-    to_hash = node.chain_code() + binascii.unhexlify(node.private_key())
+    to_hash = node.chain_code() + node.private_key()
     hashed = crypto.cn_fast_hash(to_hash)
     keys = monero.generate_monero_keys(hashed)  # spend_sec, spend_pub, view_sec, view_pub
-    creds = monero.AccountCreds.new_wallet(keys[2], keys[1], network_type)
+    creds = monero.AccountCreds.new_wallet(keys[2], keys[0], network_type)
     return creds
 
