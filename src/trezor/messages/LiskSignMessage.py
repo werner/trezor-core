0234c19a2e3d843aa36087f9b038848d70150de2
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-26 00:34:38 +0200

    pb: update

--- src/trezor/messages/LiskSignMessage.py
+++ src/trezor/messages/LiskSignMessage.py
@@ -7,17 +7,17 @@ if __debug__:
         List = None
 
 
-class StellarGetPublicKey(p.MessageType):
-    MESSAGE_WIRE_TYPE = 200
+class LiskSignMessage(p.MessageType):
+    MESSAGE_WIRE_TYPE = 118
     FIELDS = {
         1: ('address_n', p.UVarintType, p.FLAG_REPEATED),
-        2: ('show_display', p.BoolType, 0),
+        2: ('message', p.BytesType, 0),
     }
 
     def __init__(
         self,
         address_n: List[int] = None,
-        show_display: bool = None
+        message: bytes = None
     ) -> None:
         self.address_n = address_n if address_n is not None else []
-        self.show_display = show_display
+        self.message = message
--- src/trezor/messages/StellarAddress.py
+++ src/trezor/messages/StellarAddress.py
@@ -2,14 +2,14 @@
 import protobuf as p
 
 
-class MoneroAddress(p.MessageType):
-    MESSAGE_WIRE_TYPE = 331
+class StellarAddress(p.MessageType):
+    MESSAGE_WIRE_TYPE = 208
     FIELDS = {
-        1: ('address', p.BytesType, 0),
+        1: ('address', p.UnicodeType, 0),
     }
 
     def __init__(
         self,
-        address: bytes = None
+        address: str = None
     ) -> None:
         self.address = address
