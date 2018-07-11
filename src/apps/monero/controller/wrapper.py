3151d9e067a04f40c97a23817f858fa5b0e791b8
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-11 17:10:26 +0200

    xmr: controller.wrapper - ledger compatible credentials generation

--- src/apps/monero/controller/wrapper.py
+++ src/apps/monero/controller/wrapper.py
@@ -3,7 +3,7 @@
 # Author: Dusan Klinec, ph4r05, 2018
 
 
-MONERO_CURVE = 'ed25519-keccak'
+MONERO_CURVE = 'secp256k1'  # 'ed25519-keccak'
 
 
 async def monero_get_creds(ctx, address_n=None, network_type=None):
@@ -14,9 +14,10 @@ async def monero_get_creds(ctx, address_n=None, network_type=None):
 
     address_n = address_n or ()
     node = await seed.derive_node(ctx, address_n, MONERO_CURVE)
-    to_hash = node.chain_code() + node.private_key()
-    hashed = crypto.cn_fast_hash(to_hash)
-    keys = monero.generate_monero_keys(hashed)  # spend_sec, spend_pub, view_sec, view_pub
+
+    key_seed = crypto.cn_fast_hash(node.private_key())
+    keys = monero.generate_monero_keys(key_seed)  # spend_sec, spend_pub, view_sec, view_pub
+
     creds = AccountCreds.new_wallet(keys[2], keys[0], network_type)
     return creds
 
