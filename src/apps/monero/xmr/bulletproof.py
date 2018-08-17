b517906c0a980bb57e590bc909c8c446937db4fb
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-17 03:41:08 +0200

    xmr: bp - gc() during inversion

--- src/apps/monero/xmr/bulletproof.py
+++ src/apps/monero/xmr/bulletproof.py
@@ -83,10 +83,14 @@ def copy_vector(dst, src):
 def extended_gcd(aa, bb):
     lastremainder, remainder = abs(aa), abs(bb)
     x, lastx, y, lasty = 0, 1, 1, 0
+    gc_ctr = 0
     while remainder:
         lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
         x, lastx = lastx - quotient * x, x
         y, lasty = lasty - quotient * y, y
+        gc_ctr += 1
+        if gc_ctr % 10:
+            gc.collect()
     return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
 
 
