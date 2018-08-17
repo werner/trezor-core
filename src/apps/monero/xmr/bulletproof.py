4e55053fafa4ee4df98803b15ba9cd4570156c3e
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-17 14:01:09 +0200

    xmr: bp - code cleanup

--- src/apps/monero/xmr/bulletproof.py
+++ src/apps/monero/xmr/bulletproof.py
@@ -76,42 +76,6 @@ def copy_vector(dst, src):
         copy_key(dst[i], src[i])
 
 
-def extended_gcd(aa, bb):
-    lastremainder, remainder = abs(aa), abs(bb)
-    x, lastx, y, lasty = 0, 1, 1, 0
-    gc_ctr = 0
-    while remainder:
-        lastremainder, (quotient, remainder) = (
-            remainder,
-            divmod(lastremainder, remainder),
-        )
-        x, lastx = lastx - quotient * x, x
-        y, lasty = lasty - quotient * y, y
-        gc_ctr += 1
-        if gc_ctr % 10:
-            gc.collect()
-    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
-
-
-def modinv(a, m):
-    g, x, y = extended_gcd(a, m)
-    if g != 1:
-        raise ValueError
-    return x % m
-
-
-def mul_inverse(x, n):
-    return pow(x, n - 2, n)
-
-
-mul_inverse_used = modinv
-try:
-    pow(2, 5, 7)
-    mul_inverse_used = mul_inverse
-except NotImplementedError:
-    pass
-
-
 def invert(dst, x):
     """
     Modular inversion mod curve order.
