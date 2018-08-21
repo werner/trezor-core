33c5261491123f768e65d66af97f4b72a4414971
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-21 21:56:06 +0200

    xmr: test for inversion added

--- tests/test_apps.monero.crypto.py
+++ tests/test_apps.monero.crypto.py
@@ -136,6 +136,20 @@ class TestMoneroCrypto(unittest.TestCase):
         )
         self.assertEqual(crypto.encodepoint(crypto.gen_H()), H)
 
+    def test_sc_inversion(self):
+        res = crypto.new_scalar()
+        inp = crypto.decodeint(
+            unhexlify(
+                b"3482fb9735ef879fcae5ec7721b5d3646e155c4fb58d6cc11c732c9c9b76620a"
+            )
+        )
+
+        crypto.sc_inv_into(res, inp)
+        self.assertEqual(
+            hexlify(crypto.encodeint(res)),
+            b"bcf365a551e6358f3f281a6241d4a25eded60230b60a1d48c67b51a85e33d70e",
+        )
+
     def test_wallet_addr(self):
         addr = encode_addr(
             net_version(),
