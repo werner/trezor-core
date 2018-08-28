c796557cdbdcce89743ee0f1663f1a36638c8a69
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-28 19:09:57 +0200

    xmr: sign_tx logging

--- src/apps/monero/sign_tx.py
+++ src/apps/monero/sign_tx.py
@@ -12,7 +12,7 @@ async def layout_sign_tx(state, ctx, msg):
     gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
     log.debug(
         __name__,
-        "############################ TSX. Free: {} Allocated: {} thr: {}".format(
+        "############################ TSX. F: {} A: {} thr: {}".format(
             gc.mem_free(), gc.mem_alloc(), gc.mem_free() // 4 + gc.mem_alloc()
         ),
     )
@@ -23,7 +23,7 @@ async def layout_sign_tx(state, ctx, msg):
 
     log.debug(
         __name__,
-        "TsxSigner. Free: {} Allocated: {}".format(gc.mem_free(), gc.mem_alloc()),
+        "TsxSigner. F: {} A: {}".format(gc.mem_free(), gc.mem_alloc()),
     )
     log.debug(__name__, "TsxState: %s", state.ctx_sign)
     gc.collect()
